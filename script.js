5e71f939c2cecc8ce652d0d0a37a2dab7bccf9bd
m-triple-m <triplem656@gmail.com>
2023-05-31 12:17:05 +0530

    ..

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const url = 'http://localhost:5000';
 
+function getParameterByName(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
 const fetchDataFromBackend = () => {
   const response = fetch('http://localhost:5000/marker/getbyid/6476e7ea204f93edcb382785');
   response
